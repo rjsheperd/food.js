74901edd688a336d9975a959a0d65fac74ed18f2
OpenShift guest <rjsheperd@berkeley.edu>
2013-07-15 14:37:41 -0400

    prototyping json

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,28 @@ angular.module('food.jsApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.reservations = [
+        {
+            'name': 'Harlold',
+            'number': '555',
+            'email': 'harold@gmail',
+            'date': 'Tues',
+            'time': '4'
+        },
+        {
+            'name': 'Josh',
+            'number': '234',
+            'email': 'spidey@gmail',
+            'date': 'Wed',
+            'time': '5'
+        },
+        {
+            'name': 'Mary',
+            'number': '593',
+            'email': 'mary@facebook',
+            'date': 'Tues',
+            'time': '1'
+        }
+        
+    ];
   });
